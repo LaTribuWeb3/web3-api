b09c70509e42c7185db4d50cfa8126cf0fcfde04
Alfulinku <Alfulinku@gmail.com>
2023-06-02 15:38:42 +0200

    add needed headers to pathfinder

--- src/aggregators/OneInchPathfinderAggregator.ts
+++ src/aggregators/OneInchPathfinderAggregator.ts
@@ -13,13 +13,29 @@ export class OneInchPathfinderAggregator extends AggregatorInterface {
     const chainid = GetChainIdFromNetwork(network);
 
     const callUrl =
-      `https://pathfinder.1inch.io/v1.4/chain/${chainid}/router/v5/quotes?fromTokenAddress=${tokenInAddress}` +
+      `https://pathfinder-api.1inch.io/v1.4/chain/${chainid}/router/v5/quotes?fromTokenAddress=${tokenInAddress}` +
       `&toTokenAddress=${tokenOutAddress}&amount=${tokenInAmount}&preset=maxReturnResult&gasPrice=${gasPrice}`;
-
-    const quoteResponse = await axios.get(callUrl);
-    return {
-      amountOut: quoteResponse.data.bestResult.toTokenAmount.toString(),
-      aggregator: '1inch'
+    const axiosConfig = {
+      headers: {
+        Referer: 'https://app.1inch.io',
+        'user-agent': 'PostmanRuntime/7.32.2'
+      }
     };
+    try {
+      const quoteResponse = await axios.get(callUrl, axiosConfig);
+      return {
+        amountOut: quoteResponse.data.bestResult.toTokenAmount.toString(),
+        aggregator: '1inch'
+      };
+    } catch (e: any) {
+      if (e.response && e.response.data && e.response.data.err == 'all quoteResults failed') {
+        return {
+          amountOut: '0',
+          aggregator: '1inch'
+        };
+      }
+
+      throw e;
+    }
   }
 }
